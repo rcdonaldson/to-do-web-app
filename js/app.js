8c46e079c070e57181ebdc61d6b81c4bec1bdfd8
rcdonaldson <ryancdonaldson@gmail.com>
2017-03-03 11:41:40 -0500

    Date feature update
    Added datepicker and jquery CDN, styling. still errors

--- js/app.js
+++ js/app.js
@@ -6,9 +6,26 @@ controller('todoController',['$scope',function($scope){
     $scope.todos.push({'title':$scope.newTodo,'done':false})
     $scope.newTodo = ''
   }
+  $scope.editTodo = function() {
+  
+  }
   $scope.clearCompleted = function(){
     $scope.todos = $scope.todos.filter(function(item){
       return !item.done
     })
   }
 }])
+
+$('#datetimepicker').data("DateTimePicker").FUNCTION()
+//$(function () {
+//	$('#datetimepicker').datetimepicker();
+//});
+
+
+
+//-edit text - ng-bind?? ng-model
+//add, delete, edit - 3 option push
+//-date picker
+//-local storage 
+//-competed, 2 ng-repeat with filter true/false
+// load stored app data at start of app
