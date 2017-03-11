16efff0714da3016e2ae00d8bee89dc0ecef68d7
rcdonaldson <ryancdonaldson@gmail.com>
2017-03-11 11:09:21 -0500

    added filter and localStorage code

--- js/app.js
+++ js/app.js
@@ -1,16 +1,54 @@
+//.run( ($rootScope, metaData) => {
+//    // We'll inject our constant 'metaData' into the rootScope so that is accessible
+//    // in our index.html
+//    $rootScope.metaData = metaData
+//
+//    // We will test the support for localStorage once at startup and then cache the
+//    // result for later use by our service component.
+//    try {
+//        localStorage.setItem("testKey", "this is the value to be saved")
+//        localStorage.removeItem("testKey")
+//        $rootScope.haslocalStorageSupport = true
+//    }
+//    catch(error){
+//        $rootScope.haslocalStorageSupport = false
+//    }
+//
+//  } )
+
 angular.module('ToDo', []).
 controller('todoController',['$scope',function($scope){
   $scope.todos = [];
+	
+//	Local Storage
+//  var todoList = {
+//	title: title,
+//	date: date
+//  }
+//  if(localStorage.getItem('todoList') === null {
+//	var todoList = [];
+//	todoList.push(todoList);
+//	localStorage.setItem('todoLIst', JSON.stringify(todoList));
+//    }  else {
+//        var todoList = JSON.parse(localStorage.getItem('todoList'));
+//        todoList.push(todoList);
+//        localStorage.setItem('todoLIst', JSON.stringify(todoList));
+//}
 
+//	Add To Do
   $scope.addTodo = function(){
     $scope.todos.push({
 		'title':$scope.newTodo,'done':false,
-		'date':$scope.newDate,'done':false
+		'date':$scope.newDate
 	})
     $scope.newTodo = '',
 	$scope.newDate = ''
+//	$scope.newTodo.id = cuid()
+//	$scope.todoList.push($scope.newTodo)
+//	$scope.syncLocalStorage()
   }
 
+//  Edit To Do
   $scope.editTodo = function() {
 	var id = $scope.id;
 	var record = $scope.todos.$getRecord(id);
@@ -21,32 +59,26 @@ controller('todoController',['$scope',function($scope){
 	$scope.todos.$save(record).then(function(ref) {
 		var id = ref.key();
 	});
+//	$scope.syncLocalStorage()
   }
 
 //  $scope.removeTodo = function(todo){
 //	$scope.todos.$remove(todo);
+//	$scope.syncLocalStorage()
 //  }
+  
+//  Completed
   $scope.clearCompleted = function(){
     $scope.todos = $scope.todos.filter(function(item){
     	return !item.done
     })
+//	$scope.syncLocalStorage()
   }
-
-  //Local Storage
-  var todoList = {
-	title: title,
-	date: date
-  }
-  if(localStorage,getItem('todoList') === null {
-	var todoList = [];
-	todoList.push(todoList);
-	localStorage.setItem('todoLIst', JSON.stringify(todoList));
-    }  else {
-        var todoList = JSON.parse(localStorage.getItem('todoList'));
-        todoList.push(todoList);
-        localStorage.setItem('todoLIst', JSON.stringify(todoList));
-}
+  
 }]);
+
+
+
 
 
 //-edit text - ng-bind?? ng-model
